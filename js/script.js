9dbdc050cd21981a77e5c39b00d2dca94a841752
Tommaso P <tommaso.plinio@gmail.com>
2021-03-19 15:55:33 +0100

    start

---
+++ js/script.js
@@ -0,0 +1,38 @@
+// Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. Stampare a schermo la bici con peso minore
+
+// 1 creare un array di oggetti Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+
+let biciclette = [
+  {
+  'nome' : 'atala',
+  'peso' : '6000',
+  },
+  {
+  'nome' : 'bottecchia',
+  'peso' : '5000',
+  },
+  {
+  'nome' : 'rider',
+  'peso' : '8000',
+  },
+
+];
+
+
+// 2 Stampare a schermo la bici con peso minore
+let biciLeggera = biciclette[0];
+
+for (var i = 0; i < biciclette.length; i++) {
+  if (biciclette[i].peso < biciLeggera[0]) {
+    biciLeggera = biciclette[i];
+  }
+
+}
+
+let string = 'atala';
+let stringtwo = 6000;
+
+const {nome, peso} = biciLeggera;
+
+console.log(nome, peso);
+console.log(`${string}  ${6000}`) ;
